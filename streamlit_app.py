fae52c042df4a2ab721b11c2d523d6962f293fe3
jasnow.pt <114095569+jasnow-pt@users.noreply.github.com>
2022-09-22 14:17:20 +0100

    Update streamlit_app.py
    New header added

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smotthie')
 streamlit.text('🐔 Hard-Boiled Free-Rane Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
